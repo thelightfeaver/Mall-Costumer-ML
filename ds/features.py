9dd4e64c5396f692a974e6b215c54d2f20b8edb3
katuishi <diogenes_mj@hotmail.com>
2026-03-19 20:25:30 -0400

    fea: implement data cleaning and feature renaming in the processing pipeline

--- ds/features.py
+++ ds/features.py
@@ -14,8 +14,31 @@ def main(
     input_path: Path = INTERIM_DATA_DIR / "raw_interim.csv",
     output_path: Path = PROCESSED_DATA_DIR / "features.csv",
 ):
+    # load interim dataset
     df = pd.read_csv(input_path)
     logger.info(f"Interim dataset loaded with shape: {df.shape}")
+
+    # Clean dataset
+    logger.info("Cleaning dataset...")
+    df.dropna(inplace=True)
+    logger.info(f"Dataset shape after cleaning: {df.shape}")
+
+    # Drop unnecessary columns
+    df.drop(columns=["CustomerID"], inplace=True)
+
+    # Rename columns
+    logger.info("Renaming columns...")
+    df.rename(columns={
+        "Gender": "gender",
+        "Age": "age",
+        "Annual Income (k$)": "annual_income",
+        "Spending Score (1-100)": "score",
+    }, inplace=True)
+
+    
+    df.to_csv(output_path, index=False)
+    logger.success(f"Features saved to {output_path}")
+    
 
 if __name__ == "__main__":
     app()
