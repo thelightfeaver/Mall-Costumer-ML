8e7a41c90a04ed12dead2fd5603afd1dd0f61339
katuishi <diogenes_mj@hotmail.com>
2026-03-24 19:47:18 -0400

    refactor: update model training process to use pipeline and improve logging

--- ds/modeling/train.py
+++ ds/modeling/train.py
@@ -6,7 +6,8 @@ import typer
 from loguru import logger
 from sklearn.cluster import KMeans
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import LabelEncoder
 from tqdm import tqdm
 
 from ds.config import MODELS_DIR, PROCESSED_DATA_DIR
@@ -22,19 +23,23 @@ def main(
     model_path: Path = MODELS_DIR / "model.pkl",
     # -----------------------------------------
 ):
+    logger.info("Loading data...")
     df = pd.read_csv(features_path)
-    kmn = KMeans(n_clusters=3, random_state=42)
-    logger.info("Training model...")
-
     lr = LabelEncoder()
     df["gender"] = lr.fit_transform(df["gender"])
     X_train, X_test = train_test_split(df, test_size=0.2, random_state=42)
-
     X_test.to_csv(data_test_path, index=False)
 
-    kmn.fit(X_train)
+    logger.info("Data loaded and split into training and test sets.")
+    logger.info("Initializing KMeans with specified parameters...")
+    kmn = KMeans(n_clusters=3, random_state=42, max_iter=1000)
+    pipe = make_pipeline(kmn)
+
+    logger.info("Training model...")
+
+    pipe.fit(X_train)
     logger.success("Model training complete.")
-    jp.dump(kmn, model_path)
+    jp.dump(pipe, model_path)
     logger.info(f"Model saved to {model_path}.")
 
 
